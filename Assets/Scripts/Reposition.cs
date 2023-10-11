440676fabca02f8ae2fa3f6cf289377a7096b3cf
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-11 16:44:27 +0900

    [Add] 무한스크롤링 스크립트 추가

---
+++ Assets/Scripts/Reposition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void LateUpdate() //후처리 용도
+    {
+        
+    }
+}
