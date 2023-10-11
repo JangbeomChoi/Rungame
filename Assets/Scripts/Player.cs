63f640b29975e708fcbd880b1801ba7f98871b64
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-11 15:51:02 +0900

    [Update] 플레이어 스크립트 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,15 +15,42 @@ public class Player : MonoBehaviour
     public float jumpPower;
     public bool isGround;
 
+    Rigidbody2D rb;
+    Animator anim;
 
-    void Start()
+    void Awake()
     {
-        
+        //√ ±‚»≠
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * startJumpPower, ForceMode2D.Impulse);
+            
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(!isGround) 
+        {
+            ChangeAnim(State.Run);
+        }
         
+        isGround = true;
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        ChangeAnim(State.Jump);
+        isGround = false;
+    }
+    void ChangeAnim(State state)
+    {
+        anim.SetInteger("State", (int)state);
     }
 }
