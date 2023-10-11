aaa161c37a274a9ba72b1f784f67dcceece13267
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-11 18:39:11 +0900

    [Add] change scripts 추가

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class Reposition : MonoBehaviour
 {
+    public UnityEvent onMove;
     void LateUpdate() //후처리 용도
     {
         if (transform.position.x > -10)
             return;
 
         transform.Translate(24,0,0, Space.Self); 
+        onMove.Invoke();
     }
 }
