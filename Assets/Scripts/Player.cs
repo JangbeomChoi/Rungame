513c9bbf860882563674d65ae57db1dabcb5605f
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-11 15:24:12 +0900

    [Add] player script 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum State 
+    {
+        Stand,
+        Run,
+        Jump,
+        Hit,
+    }
+    public float startJumpPower;
+    public float jumpPower;
+    public bool isGround;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
